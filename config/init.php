a284fd6d238459cb8334fbc99ef73fe5b04e6b8e
Olexandr Shaposhnyk <olexandrshaposhnyk@Olexandrs-MacBook-Pro.local>
2023-01-03 02:03:03 +0200

    set project structure

---
+++ config/init.php
@@ -0,0 +1,17 @@
+<?php
+
+define("DEBUG", 1);
+define("ROOT", dirname(__DIR__));
+define("PUBLIC", ROOT . '/public');
+define("APP", ROOT . '/app');
+define("CORE", ROOT . '/core');
+define("HELPERS", CORE . '/helpers');
+define("CACHE", ROOT . '/tmp/cache');
+define("LOGS", ROOT . '/tmp/logs');
+define("CONFIG", ROOT . '/config');
+define("LAYOUT", 'main');
+define("PATH", 'https://127.0.0.1:8000');
+define("ADMIN", 'https://127.0.0.1:8000/admin');
+define("NO_IMAGE", 'uploads/no-image.jpg');
+
+require_once ROOT . '/vendor/autoload.php';
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+if (PHP_MAJOR_VERSION < 8) {
+    die('Minimum required PHP version is 8.0.0');
+}
+
+require_once dirname(__DIR__) . '/config/init.php';
